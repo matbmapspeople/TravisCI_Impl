8862c32743745176b541842d81b5b3dcfdb52ca6
Banaszake <mateusz.banaszak5303@gmail.com>
2022-01-19 14:45:03 +0100

    ss

--- test.js
+++ test.js
@@ -18,3 +18,9 @@ describe("Filter function", () => {
   
     });
   });
+
+  describe("Calculator tests", () => {
+    test('adding 1 + 2 should return 3', () => {
+      expect(mathOperations.sum(1, 2)).toBe(3);
+    });
+   })
