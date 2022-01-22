a74369eb93e3432c5f83711c8aa42d0894fdb4a3
mateu <mateusz.banaszak5303@gmail.com>
2022-01-22 15:41:44 +0100

    ch

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ function filterByTerm(inputArr, searchTerm) {
     });
   }
 
-describe("Filter function", () => {
-    test("it should filter by a search term (link)", () => {
+describe("Filter specific name", () => {
+    test("It should successfuly filter link word: link", () => {
       const input = [
         { id: 1, url: "https://www.url1.dev" },
         { id: 2, url: "https://www.url2.dev" },
@@ -21,7 +21,7 @@ describe("Filter function", () => {
     });
   });
 
-  describe("Calculator tests", () => {
+  describe("Calculator test", () => {
     test('adding 1 + 2 should return 3', () => {
       expect(mathOperations.sum(1, 2)).toBe(3);
     });
