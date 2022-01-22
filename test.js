9a8acf8abc3c00c039558d9e3ad2f971a5a46ae5
mateu <mateusz.banaszak5303@gmail.com>
2022-01-22 16:03:06 +0100

    chanegs

--- test.js
+++ test.js
@@ -1,20 +1,33 @@
-function filterByTerm(inputArr, searchTerm) {
-    return inputArr.filter(function(arrayElement) {
-      return arrayElement.url.match(searchTerm);
-    });
-  }
+const mathOperations = require("./somemath");
 
-describe("Filter function", () => {
-    test("it should filter by a search term (link)", () => {
-      const input = [
-        { id: 1, url: "https://www.url1.dev" },
-        { id: 2, url: "https://www.url2.dev" },
-        { id: 3, url: "https://www.link3.dev" }
-      ];
-  
-      const output = [{ id: 3, url: "https://www.link3.dev" }];
-  
-      expect(filterByTerm(input, "link")).toEqual(output);
-  
-    });
+function filterByTerm(inputArr, searchTerm) {
+  return inputArr.filter(function (arrayElement) {
+    return arrayElement.url.match(searchTerm);
   });
+}
+
+describe("Filter word function", () => {
+  test("It should filter word 'link'", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+
+    const output = [{ id: 3, url: "https://www.link3.dev" }];
+
+    expect(filterByTerm(input, "link")).toEqual(output);
+
+  });
+});
+
+describe("Sum test", () => {
+  test('By adding 1 and 2 we shoould get 3', () => {
+    expect(mathOperations.sum(1, 2)).toBe(3);
+  });
+})
+
+describe("Multiplication test" , () => {
+  test('By multypling 5,154 and 678,1223 we should get 3.495,0423342')
+  expect(mathOperations.mult(5,154, 678,1223)).toBe(3495.0423342)
+})
