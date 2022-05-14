abebdb35f363d473b706380534e833d6beb5230f
Banaszake <mateusz.banaszak5303@gmail.com>
2022-05-14 11:05:39 +0200

    changes

---
+++ AngularCourseENG/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styles: [
+    `
+    p {
+      color: green;
+    }
+    `]
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
