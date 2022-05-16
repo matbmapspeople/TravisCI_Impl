405c7fc1262b0d58941e2c78585e01fc434d2a94
Banaszake <mateusz.banaszak5303@gmail.com>
2022-05-16 13:02:30 +0200

    Events

--- AngularCourseENG/src/app/app.module.ts
+++ AngularCourseENG/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { ServersComponent } from './servers/servers.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
     
     
   ],
