d02a3a7accbedba635d55607ab58ed9007d02e6f
Banaszake <mateusz.banaszak5303@gmail.com>
2022-05-18 12:57:29 +0200

    end of section 6

--- ShoppingListProjectv1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingListProjectv1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'Test', 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/_DSC6387.jpg?itok=8FYVA3H9&mtime=1629703604' )
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
