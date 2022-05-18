8b540e2d02c763a1ced3b442e867f51640dccef5
Banaszake <mateusz.banaszak5303@gmail.com>
2022-05-18 09:14:31 +0200

    alias changes

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name: 'testserver', content: 'sth'}];
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(serverBlueprint: {serverName: string, serverContent: string}) {
+     this.serverElements.push({
+      type: 'blueprint',
+      name: serverBlueprint.serverName,
+      content: serverBlueprint.serverContent
+    });
+  }
+  
 }
+
